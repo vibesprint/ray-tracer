491d2e7e2a6a3c4492615acca58f2f93e5263688
sparrow <admin@orangubaku>
2021-06-10 19:28:44 +0530

    added lightning function

--- light.py
+++ light.py
@@ -1,6 +1,46 @@
-
+import color
+import base
 
 class point_light:
     def __init__(self, position, intensity):
         self.intensity = intensity
         self.position = position
+
+
+def reflect(vect, normal):
+    return base.sub(
+            vect,
+            base.scalar_mul(
+                normal,
+                2 * base.dot(vect, normal)
+                )
+            )
+
+
+
+def lightning(material, light, point, eyev, normalv):
+    effective_color = color.hadamard_mul(material.color, light.intensity)
+    lightv = base.normalize( base.sub(light.position, point) )
+    ambient = color.scalar_mul(effective_color, material.ambient)
+    diffuse = None
+    specular = None
+
+    light_dot_normal = base.dot(lightv, normalv)
+
+    if light_dot_normal < 0:
+        diffuse = color.color(0, 0, 0)
+        specular = color.color(0, 0, 0)
+
+    else:
+        diffuse = color.scalar_mul(effective_color, material.diffuse * light_dot_normal)
+
+        reflectv = reflect(base.negate(lightv), normalv)
+        reflect_dot_eye = base.dot(reflectv, eyev)
+
+        if reflect_dot_eye < 0:
+            specular = color.color(0, 0, 0)
+        else:
+            factor = reflect_dot_eye ** material.shininess
+            specular = color.scalar_mul(light.intensity, material.specular * factor)
+
+    return color.add(ambient, color.add(diffuse, specular))
--- test_light.py
+++ test_light.py
@@ -1,6 +1,9 @@
 import light
 import color
 import base
+import material
+
+import math
 
 def test_point_light():
     position = base.point(0, 0, 0)
@@ -8,3 +11,58 @@ def test_point_light():
     lht = light.point_light(position, intensity)
     assert base.equals(lht.position, position)
     assert base.equals(lht.intensity, intensity)
+
+
+def lightning_test_setup():
+    m = material.material()
+    position = base.point(0, 0, 0)
+    return m, position
+
+
+def test_lightning():
+    """Eye between the light and the surface"""
+    m, position = lightning_test_setup()
+    eyev = base.vector(0, 0, -1)
+    normalv = base.vector(0, 0, -1)
+    lght = light.point_light(base.point(0, 0, -10), color.color(1, 1, 1))
+    result = light.lightning(m, lght, position, eyev, normalv)
+    print(f"[+] Result {result}")
+    assert base.equals(result, color.color(1.9, 1.9, 1.9))
+
+def test_lightning2():
+    """Eye between light and the surface, eye offset 45 degrees"""
+    m, position = lightning_test_setup()
+    const = math.sqrt(2) / 2
+    eyev = base.vector(0, const, -const)
+    normalv = base.vector(0, 0, -1)
+    lght = light.point_light(base.point(0, 0, -10), color.color(1, 1, 1))
+    result = light.lightning(m, lght, position, eyev, normalv)
+    print(f"[+] Result 2: {result}")
+    assert base.equals(result, color.color(1, 1, 1))
+
+def test_lightning3():
+    """light offset 45 degrees"""
+    m, position = lightning_test_setup()
+    eyev = base.vector(0, 0, -1)
+    normalv = base.vector(0, 0, -1)
+    lght = light.point_light(base.point(0, 10, -10), color.color(1, 1, 1))
+    result = light.lightning(m, lght, position, eyev, normalv)
+    assert base.equals(result, color.color(0.7364, 0.7364, 0.7364))
+
+def test_lightning4():
+    """eye in the path of reflection vector"""
+    m, position = lightning_test_setup()
+    const = math.sqrt(2) / 2
+    eyev = base.vector(0, -const, -const)
+    normalv = base.vector(0, 0, -1)
+    lght = light.point_light(base.point(0, 10, -10), color.color(1, 1, 1))
+    result = light.lightning(m, lght, position, eyev, normalv)
+    assert base.equals(result, color.color(1.6364, 1.6364, 1.6364))
+
+def test_lightning5():
+    m, position = lightning_test_setup()
+    eyev = base.vector(0, 0, -1)
+    normalv = base.vector(0, 0, -1)
+    lght = light.point_light(base.point(0, 0, 10), color.color(1, 1, 1))
+    result = light.lightning(m, lght, position, eyev, normalv)
+    assert base.equals(result, color.color(0.1, 0.1, 0.1))
