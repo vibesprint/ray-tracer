776b5463c6d87576450d74f7c5874d0b5f87bab5
sparrow <admin@orangubaku>
2021-07-11 09:28:17 +0530

    added a print statement

--- obj_demo.py
+++ obj_demo.py
@@ -22,6 +22,7 @@ def get_model():
 
     print(f'[*] Parsing the model file ...')
     parse_res = objparser.parse_obj_file(file)
+    print(f"[*] No. of lines ignored: {parse_res.ignored_lines}")
     print(f"[+] Parsing done")
     return objparser.obj_to_group(parse_res)
 
